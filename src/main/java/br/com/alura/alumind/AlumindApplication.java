aefb0c1d0d77d92208ce3d14e099955209ae7222
Crisley Marques <crisleyvmarques@gmail.com>
2024-09-17 21:15:41 -0300

    first commit

---
+++ src/main/java/br/com/alura/alumind/AlumindApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.alumind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/alumind/AlumindApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.alumind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
